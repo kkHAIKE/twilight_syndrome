49b4e2e6ee5b987dcf0b6f4dfad17c48ac44c18e
kkhaike <kkhaike@gmail.com>
2022-03-18 13:41:02 +0800

    cdb 支持大 size

--- cdb.py
+++ cdb.py
@@ -1,5 +1,6 @@
 import struct
 import sys
+import io
 
 class CDB:
     def __init__(self, fpath):
@@ -28,16 +29,39 @@ class CDB:
         return self._f.read(self.head[idx][1] * 2048)
 
     def write(self, idx, data):
-        n = (len(data) + 2047) // 2048
+        # pad
+        if len(data) % 2048 != 0:
+            data += b'\0' * (2048 - (len(data) % 2048))
+
+        n = len(data) // 2048
         on = self.head[idx][1]
         print(on, "->", n)
-        assert n <= on, "{},{}".format(n, on)
 
         self._seek(idx)
+
+        ext = None
+        if n > on:
+            # 扩张，读取后面的
+            self._f.seek(on*2048, io.SEEK_CUR)
+            ext = self._f.read()
+            self._seek(idx)
+
         self._f.write(data)
 
         for _ in range(on*2048 - len(data)):
             self._f.write(b'\0')
+
+        if ext is not None:
+            self._f.write(ext)
+
+            # 修正头部
+            self._f.seek(0)
+            for p, l in self.head:
+                if p > self.head[idx][0]:
+                    p += n - on
+                elif p == self.head[idx][0]:
+                    l = n
+                self._f.write(struct.pack("<2H", p, l))
 
 def main(argv):
     db = CDB(argv[1])
