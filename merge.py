36075f641c1a50fe35fac971a0e47413f4dcf82f
kkhaike <kkhaike@gmail.com>
2022-03-19 12:13:53 +0800

    美化json输出

--- merge.py
+++ merge.py
@@ -3,6 +3,7 @@ from ini import Ini
 import re
 import os
 import json
+import ctypes
 
 def readline(f: io.TextIOWrapper):
     arr = []
@@ -100,23 +101,67 @@ def headfix(line, di):
             line = line[:m.start(1)] + str(n2) + line[m.end(1):]
     return line
 
+class NoIndent(object):
+    """ Value wrapper. """
+    def __init__(self, value):
+        self.value = value
+
+class MyEncoder(json.JSONEncoder):
+    FORMAT_SPEC = '@@{}@@'
+    regex = re.compile(FORMAT_SPEC.format(r'(\d+)'))
+
+    def __init__(self, **kwargs):
+        # Save copy of any keyword argument values needed for use here.
+        self.__sort_keys = kwargs.get('sort_keys', None)
+        super(MyEncoder, self).__init__(**kwargs)
+
+    def default(self, obj):
+        return (self.FORMAT_SPEC.format(id(obj)) if isinstance(obj, NoIndent)
+                else super(MyEncoder, self).default(obj))
+
+    def encode(self, obj):
+        format_spec = self.FORMAT_SPEC  # Local var to expedite access.
+        json_repr = super(MyEncoder, self).encode(obj)  # Default JSON.
+
+        # Replace any marked-up object ids in the JSON repr with the
+        # value returned from the json.dumps() of the corresponding
+        # wrapped Python object.
+        for match in self.regex.finditer(json_repr):
+            # see https://stackoverflow.com/a/15012814/355230
+            id = int(match.group(1))
+            no_indent = ctypes.cast(id, ctypes.py_object).value
+            json_obj_repr = json.dumps(no_indent.value,
+                sort_keys=self.__sort_keys,
+                ensure_ascii=False,
+            )
+
+            # Replace the matched id string with json formatted representation
+            # of the corresponding Python object.
+            json_repr = json_repr.replace(
+                            '"{}"'.format(format_spec.format(id)), json_obj_repr)
+
+        return json_repr
+
 def mergeLink(src, dst, lines):
     with open(src, "rt", encoding='utf-8') as f:
         data = json.load(f)
 
     di = 0
     for v in data:
-        for vv in v:
+        for i, vv in enumerate(v):
             if vv[0] == "TEXT":
                 line = lines[di].rstrip("\r\n")
                 line = headfix(line, di)
 
                 vv[1] = line
                 di += 1
+            # 缩进修正
+            v[i] = NoIndent(vv)
     assert di == len(lines)
 
+    js = json.dumps(data, cls=MyEncoder, indent=2, ensure_ascii=False)
     with open(dst, "wt", encoding='utf-8') as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+        f.write(js)
 
 # 合并 raw 和 cn 文件到脚本
 # 并且自动检查错误，和替换 HEAD
